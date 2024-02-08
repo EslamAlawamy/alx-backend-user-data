586c90609887579ae4ff9aba845fb472bf6518f7
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2024-02-09 01:16:03 +0200

    Create encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Password encryption module """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Create hashed password """
+    pwd = password.encode()
+    hashed_pwd = bcrypt.hashpw(pwd, bcrypt.gensalt())
+    return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ if hashed password matches unhashed password """
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
